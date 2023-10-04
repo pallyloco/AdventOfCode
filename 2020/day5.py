c9f795a036c4079e1beb295633619ff7b88f7143
Sandy Bultena <sandy.bultena@sympatico.ca>
2023-10-04 16:45:57 -0400

    day 5 complete

--- 2020/day5.py
+++ 2020/day5.py
@@ -1,6 +1,7 @@
 def main(part: int = 1):
     file = open('day5_input.txt', 'r')
     bin_dict = {"F":0, "B":1,"L":0, "R":1}
+    all_seat_ids = list()
 
     max_seat_id = 0
     for line in map(str.rstrip, file):
@@ -9,7 +10,15 @@ def main(part: int = 1):
             seat_id = seat_id << 1
             seat_id += bin_dict[c]
 
+        all_seat_ids.append(seat_id)
         max_seat_id = max(max_seat_id, seat_id)
+
     print ("Max seat id is:", max_seat_id)
+    all_seat_ids.sort()
+    for i in range(1,len(all_seat_ids)-1):
+        if all_seat_ids[i+1]-all_seat_ids[i] == 2:
+            print ("My seat number is:", all_seat_ids[i]+1)
+
+
 if __name__ == '__main__':
     main(1)
