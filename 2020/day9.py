412199aa3ce77e4667908dfaf81361a8ca7bfb7a
Sandy Bultena <sandy.bultena@sympatico.ca>
2023-10-17 18:08:20 -0400

    Day 9 done

--- 2020/day9.py
+++ 2020/day9.py
@@ -1,22 +1,43 @@
 from collections import deque
 key_length = 25
+inputs = list()
 
 def main(part: int = 1):
     file = open('day9_input.txt', 'r')
+    invalid = 0
     deck = deque()
     for line in map(int, map(str.rstrip, file)):
-        if len(deck) < key_length:
-            deck.append(line)
-            continue
+        inputs.append(line)
+        if not invalid:
+            if len(deck) < key_length:
+                deck.append(line)
+                continue
 
-        if len(deck) > key_length:
-            deck.popleft()
+            if len(deck) > key_length:
+                deck.popleft()
 
-        if not is_valid(line, deck):
-            print("found invalid", line)
-            break
+            if  not is_valid(line, deck):
+                print("found invalid", line)
+                invalid = line
+
 
         deck.append(line)
+
+
+    # find weakness
+    for i in range(len(inputs)-1):
+        total = inputs[i]
+        lowest = total
+        highest = total
+        for j in range(i+1,len(inputs)):
+            total = total+inputs[j]
+            lowest = min(lowest,inputs[j])
+            highest = max(highest,inputs[j])
+            if total > invalid:
+                break
+            if total == invalid:
+                print (lowest+highest)
+                exit
 
 def is_valid(num:int, deck:deque[int]):
     for i in range(0,key_length-1):
@@ -26,6 +47,7 @@ def is_valid(num:int, deck:deque[int]):
             if deck[i] + deck[j] == num:
                 return True
     return False
+
 
 
 
