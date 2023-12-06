77e9da455f4d4050f1a56d0f9591f2a474b3ec03
Sandy Bultena <sandy.bultena@sympatico.ca>
2023-12-05 23:58:02 -0500

    2023 -day 5 part 2 works, part 1 isn't included

--- 2023/day_05.py
+++ 2023/day_05.py
@@ -1,12 +1,22 @@
-def main(part: int = 1):
+from __future__ import annotations
+from typing import Optional
+
+Range = tuple[int, int]  # start and end numbers
+
+
+def main():
     file = open("day_05_input.txt", 'r')
 
     # seeds
     _, seed_str = next(file).split(":")
-    seeds = list(map(int, seed_str.split()))
-    conversion: dict[str, dict[int, int]] = dict()
+    seed_info = list(map(int, seed_str.split()))
+    conversion: dict[str, dict[Range, Range]] = dict()
 
-    valid_source_numbers: list[int] = seeds
+    valid_source_ranges: list[Range] = list()
+    seed_source_ranges: list[Range] = list()
+    for s, l in zip(seed_info[::2], seed_info[1::2]):
+        valid_source_ranges.append(make_range(s, l))
+        seed_source_ranges.append(make_range(s, l))
 
     conversion_type = None
     for line in map(str.rstrip, file):
@@ -17,7 +27,10 @@ def main(part: int = 1):
         if line.find(":") != -1:
 
             # update any conversion numbers that were not listed
-            valid_source_numbers = update_conversion_numbers(conversion, conversion_type, valid_source_numbers)
+            if conversion_type in conversion:
+                conversion[conversion_type] = update_conversion_numbers(conversion[conversion_type],
+                                                                        valid_source_ranges)
+                valid_source_ranges = list(conversion[conversion_type].values())
 
             # setup for new conversion type
             conversion_type, _ = line.split()
@@ -25,35 +38,91 @@ def main(part: int = 1):
 
         else:
             destination_start, source_start, number_range = list(map(int, line.split()))
-            for v in valid_source_numbers:
-                if v in range(source_start, source_start + number_range):
-                    conversion[conversion_type][v] = destination_start + (v - source_start)
+            conversion[conversion_type][make_range(source_start, number_range)] = make_range(destination_start,
+                                                                                             number_range)
 
-    update_conversion_numbers(conversion, conversion_type, valid_source_numbers)
-    print(conversion)
-    locations = [get_location_from_seed(seed, conversion) for seed in seeds]
-    print("part 1:", min(locations))
+    conversion[conversion_type] = update_conversion_numbers(conversion[conversion_type], valid_source_ranges)
 
-
-def update_conversion_numbers(conversion, conversion_type, valid_source_numbers):
-    if conversion_type in conversion:
-        for v in valid_source_numbers:
-            if v not in conversion[conversion_type].keys():
-                conversion[conversion_type][v] = v
-        return list(conversion[conversion_type].values())
-    return valid_source_numbers
+    locations: list[Range] = list(conversion["humidity-to-location"].values())
+    locations.sort()
+    print("ans part 2:", start(locations[0]))
 
 
-def get_location_from_seed(seed: int, conversion: dict[str, dict[int, int]]) -> int:
-    soil: int = conversion["seed-to-soil"][seed]
-    fertilizer: int = conversion["soil-to-fertilizer"][soil]
-    water: int = conversion["fertilizer-to-water"][fertilizer]
-    light: int = conversion["water-to-light"][water]
-    temperature: int = conversion["light-to-temperature"][light]
-    humidity: int = conversion["temperature-to-humidity"][temperature]
-    location: int = conversion["humidity-to-location"][humidity]
-    print(seed, soil, fertilizer, water, light, temperature, humidity, location)
-    return location
+def update_conversion_numbers(decoder: dict[Range, Range], source_ranges: list[Range]) -> dict[Range, Range]:
+    decoder_sorted_keys: list[Range] = sorted(decoder.keys())
+    source_ranges.sort()
+    new_decoder: dict[Range:Range] = dict()
+
+    decoder_src: Optional[Range] = None
+    if len(decoder_sorted_keys):
+        decoder_src = decoder_sorted_keys.pop(0)
+
+    while len(source_ranges):
+        src: Range = source_ranges.pop(0)
+        done = False
+        while not done:
+
+            # ++++++++
+            #           ***********
+            if decoder_src is None or end(src) < start(decoder_src):
+                new_decoder[src] = src
+                done = True
+                break
+
+            #        +++++++++
+            # ****
+            if start(src) > end(decoder_src):
+                decoder_src = None
+                if len(decoder_sorted_keys):
+                    decoder_src = decoder_sorted_keys.pop(0)
+                continue
+
+            # +++++++++++
+            #           ********
+            if start(src) < start(decoder_src) <= end(src):
+                new_decoder[(start(src), start(decoder_src) - 1)] = (start(src), start(decoder_src) - 1)
+                src = (start(decoder_src), end(src))
+                continue
+
+            #    ++++++++++
+            #   ************
+            if start(src) >= start(decoder_src) and end(src) <= end(decoder_src):
+                decoder_dest = decoder[decoder_src]
+                dest_start = start(src) - start(decoder_src) + start(decoder_dest)
+                new_decoder[src] = make_range(dest_start, length(r=src))
+                done = True
+                break
+
+            #         +++++++++
+            # ************
+            if start(src) <= end(decoder_src) < end(src):
+                decoder_dest = decoder[decoder_src]
+                dest_start = start(src) - start(decoder_src) + start(decoder_dest)
+                l = end(decoder_src) - start(src) + 1
+                new_decoder[make_range(start(src), l)] = make_range(dest_start, l)
+                src = (end(decoder_src) + 1, end(src))
+                continue
+
+            print("wtf", )
+    return new_decoder
+
+
+def length(start_number: Optional[int] = None, end_number: Optional[int] = None, r: Optional[Range] = None) -> int:
+    if r is not None:
+        return end(r) - start(r) + 1
+    return end_number - start_number + 1
+
+
+def make_range(start_number: int, length_number: int) -> Range:
+    return start_number, start_number + length_number - 1
+
+
+def start(r: Range) -> int:
+    return r[0]
+
+
+def end(n: Range) -> int:
+    return n[1]
 
 
 if __name__ == "__main__":
