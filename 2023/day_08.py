566af39deb387e5c8ccd16299d86081068c954ee
Sandy Bultena <sandy.bultena@sympatico.ca>
2023-12-08 22:55:16 -0500

    2023 - day 8

--- 2023/day_08.py
+++ 2023/day_08.py
@@ -1,12 +1,18 @@
 import itertools as it
 import re
+import math
+
+right_left: list[str] = list()
+tree_info: dict[str, tuple[str, str]] = dict()
+dmap: dict[str, int] = {"R": 1, "L": 0}
 
 
 def main(part: int = 1):
+    global right_left
+    global tree_info
+
     file = open("day_08_input.txt", 'r')
     line_map = map(str.rstrip, file)
-    right_left: list[str] = list()
-    tree_info: dict[str, tuple[str, str]] = dict()
     for line in line_map:
         if line == "":
             break
@@ -14,18 +20,46 @@ def main(part: int = 1):
     for line in line_map:
         if line == "":
             continue
-        key, value = map(str.strip,line.split("="))
-        v1, v2 = re.findall(r"[A-Z]+", value)
+        key, value = map(str.strip, line.split("="))
+        v1, v2 = re.findall(r"((?:[A-Z]|[0-9])+)", value)
         tree_info[key] = (v1, v2)
 
-    location = "AAA"
-    dmap: dict[str, int] = {"R": 1, "L": 0}
-    for i, direction in enumerate(it.cycle(right_left)):
-        location = tree_info[location][dmap[direction]]
-        print(i, location)
-        if location == "ZZZ":
-            break
-    print("part 1:", i+1)
+    """
+    IMPORTANT INFORMATION ABOUT THE DATA: 
+    ====================================================
+    
+    IT IS CYCLICAL
+    xyA -> ijZ -> ijZ for all xy and ij
+    
+    ijZ -> ijZ takes exactly one cycle of the directions, for all ij
+    
+    xyA -> ijZ takes exactly n*(length of directions), for all xy
+    
+    ====================================================
+    """
+    cycle = len(right_left)
+
+    locations = [x for x in tree_info.keys() if x[-1] == 'A']
+    state: list[list] = list()
+
+    # steps to get from xyA -> ijZ -> ijZ
+    for location in locations:
+        aaa = location == "AAA"
+        z_found = False
+        for i in it.count():
+            direction = right_left[i % cycle]
+            location = tree_info[location][dmap[direction]]
+            if location[-1] == "Z" and z_found:
+                state[-1].append(i - state[-1][0])
+                break
+            if location[-1] == "Z":
+                state.append([i])
+                z_found = 3
+        if aaa:
+            print("Answer 1:", state[-1][0] + 1)
+    multiples = [s[1] // cycle for s in state]
+    lcm = math.lcm(*multiples)
+    print("answer 2: ", lcm * cycle)
 
 
 if __name__ == "__main__":
